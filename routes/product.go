165ac67eeab2075fe8fdae22d8ebd6855231a0dd
KhalidBrinto <kbsbrinto@gmail.com>
2025-06-29 23:57:57 +0600

    added api routes

--- routes/product.go
+++ routes/product.go
@@ -12,12 +12,15 @@ func ProductRoutes(router *gin.Engine) {
 	{
 		products.GET("/search", controllers.SearchProducts)
 		products.POST("/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.CreateProduct)
+		products.POST("/variation/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.CreateVariation)
 		products.GET("", controllers.GetProducts)
 		products.GET("/:id", controllers.GetSingleProductV2)
 		products.GET("/new-arrival", controllers.GetNewArrivalProducts)
 		products.GET("/trending", controllers.GetTrendingProducts)
 		products.PUT("/:id/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.UpdateProduct)
+		products.PUT("/variation/:id/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.UpdateVariation)
 		products.DELETE("/:id/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.DeleteProduct)
+		products.DELETE("/variation/:id/", middlewares.AuthMiddleware(), middlewares.CheckIfAdmin(), controllers.DeleteVariation)
 	}
 
 	productAttributes := router.Group("/api/product-attributes")
